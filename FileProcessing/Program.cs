545be1ed2c4c046899512ba1fbe000b2695fd6e5
Joseph <54252110+josephestes@users.noreply.github.com>
2022-12-07 23:20:52 -0500

    Update Program.cs
    A for loop was used instead of a while loop to read the input file.
    A foreach loop was used to iterate over the numbers in the numbers list, rather than using a foreach loop inside a while loop.
    The in keyword was used to iterate over a range of numbers, rather than using a while loop.
    String interpolation was used to make the code easier to read and understand.
    The File.ReadAllLines method was used to read the input file into a string array, rather than using a StreamReader and a while loop.

--- FileProcessing/Program.cs
+++ FileProcessing/Program.cs
@@ -28,57 +28,54 @@ namespace NumbersFileProcessing
 
             int numberIn;
             var numbers = new List<int>();
-            int index = 0;
-            int rangeCount;
 
-            //Read the input file
-            using (StreamReader reader = new StreamReader(FILENAME))
+            // Read the input file
+            string[] lines = File.ReadAllLines(FILENAME);
+
+            foreach (var line in lines)
             {
-                while (!reader.EndOfStream)
+                if (int.TryParse(line, out numberIn))
                 {
-                    if (int.TryParse(reader.ReadLine(), out numberIn))
-                        numbers.Add(numberIn);
+                    numbers.Add(numberIn);
                 }
+            }
 
-                //Create the output file
-                using (StreamWriter outFile = new StreamWriter("output.txt"))
+            // Create the output file
+            using (StreamWriter outFile = new StreamWriter("output.txt"))
+            {
+                // Output details to user
+                WriteLine($"Number Count: {numbers.Count()}");
+                WriteLine($"Smallest Number: {numbers.Min()}");
+                WriteLine($"Largest Number: {numbers.Max()}");
+                WriteLine($"Average of Numbers: {numbers.Average()}");
+
+                // Output details to file
+                outFile.WriteLine($"Number Count: {numbers.Count()}");
+                outFile.WriteLine($"Smallest Number: {numbers.Min()}");
+                outFile.WriteLine($"Largest Number: {numbers.Max()}");
+                outFile.WriteLine($"Average of Numbers: {numbers.Average()}");
+
+                // Determine total numbers within each range
+                for (int i = 0; i < RANGE_LIMIT; i += RANGE_INTERVAL)
                 {
-                    //Output details to user
-                    WriteLine("Number Count: " + numbers.Count());
-                    WriteLine("Smallest Number: " + numbers.Min());
-                    WriteLine("Largest Number: " + numbers.Max());
-                    WriteLine("Average of Numbers: " + numbers.Average());
+                    int rangeCount = 0;
 
-                    //Output details to file
-                    outFile.WriteLine("Number Count: " + numbers.Count());
-                    outFile.WriteLine("Smallest Number: " + numbers.Min());
-                    outFile.WriteLine("Largest Number: " + numbers.Max());
-                    outFile.WriteLine("Average of Numbers: " + numbers.Average());
-
-                    //Determine total numbers within each range
-                    while (index < RANGE_LIMIT)
+                    foreach (var number in numbers)
                     {
-                        rangeCount = 0;
-
-                        foreach (var number in numbers)
+                        if (number >= i && number < (i + RANGE_INTERVAL))
                         {
-                            if (number >= index && number < (index + RANGE_INTERVAL))
-                            {
-                                rangeCount = rangeCount + 1;
-                            }
+                            rangeCount++;
                         }
-
-                        //Output to user
-                        WriteLine("There are {0} numbers within the range {1} - {2}", rangeCount, index, (index + RANGE_INTERVAL - 1));
-
-                        //Output to file
-                        outFile.WriteLine("There are {0} numbers within the range {1} - {2}", rangeCount, index, (index + RANGE_INTERVAL - 1));
-
-                        index = index + RANGE_INTERVAL;
                     }
 
-                    WriteLine("Output to file");
+                    // Output to user
+                    WriteLine($"There are {rangeCount} numbers within the range {i} - {i + RANGE_INTERVAL - 1}");
+
+                    // Output to file
+                    outFile.WriteLine($"There are {rangeCount} numbers within the range {i} - {i + RANGE_INTERVAL - 1}");
                 }
+
+                WriteLine("Output to file");
             }
         }
     }
